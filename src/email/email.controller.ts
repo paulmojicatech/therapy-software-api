1a2bac8c7d2bb3c14e340f4a61a5199b672058f1
Paul <paulmojicatech@gmail.com>
2020-01-13 22:31:28 -0500

    #8 send mass email works

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,18 +1,43 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { EmailService } from 'src/services/email.service';
-
+import { DataService } from 'src/services/data.service';
+import { IClients } from 'src/models/clients.interface';
 @Controller('email')
 export class EmailController {
-    constructor (private _emailSvc: EmailService) { }
+    constructor (private _emailSvc: EmailService, private _dataSvc: DataService) { }
 
-    @Post()
-    async sendEmail(): Promise<boolean> {
+    @Post('massEmail')
+    async sendMassEmail(@Body() emailReq: { headers: any, body: { subject: string, message: string, clientsToInclude: number[] } }): Promise<boolean> {
         try {
-            const isSuccess = await this._emailSvc.sendEmail('paulmojicatech@gmail.com', 'Testing', 'New Email From Node');
-            return Promise.resolve(isSuccess);
+            const clientsWhere = { $and: [ 
+                {
+                    ClientID: {
+                        $in: emailReq.body.clientsToInclude
+                    }
+                },
+                { 
+                    ClientEmail: { 
+                        $not: { 
+                            $type: 10 // null type
+                        }, 
+                        $ne: "" // empty strings
+                    } 
+                } 
+            ]};
+
+            const clientsToSendTo = await this._dataSvc.getCollection('Clients', clientsWhere);
+            if (!!clientsToSendTo) {
+                clientsToSendTo.forEach(async client => {
+                    await this._emailSvc.sendEmail(client.ClientEmail, emailReq.body.subject, emailReq.body.message);
+                });
+            }
+
+            return Promise.resolve(true);
         } catch (ex) {
             console.log('ERR', ex);
             return Promise.resolve(false);
+        } finally {
+            this._dataSvc.disconnect();
         }
 
     }
